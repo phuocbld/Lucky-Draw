ae22676d547d4f721a1c32492240811dbb72004e
Phuoc <banhluudanhphuoc@gmail.com>
2025-12-30 00:20:21 +0700

    fix : vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: '/Lucky-Draw/',   // 🔥 BẮT BUỘC
+  plugins: [react()],
 })
+
